289ae7aa3de42222e210f31acd94eb86f95dbfee
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-23 15:01:09 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #define true 1
 #define false 0
-#define n 10
-void getting_matrix(int matrix[10][10]){
-    int i,j;
-for(i=0;i<2;i++)
-  for(j=0;j<2;j++){
-  scanf("%d",&matrix[i][j]);
-  } 
-}
-int check_path(int matrix[10][10]){
-    int k,i,j;
-int result=0;
-for(k=0; k<=10 ; k++)
- for(i=0;i<=10;i++)
-  for(j=0;j<=10;j++){
-     if (matrix[i][k] + matrix[k][j] < matrix[i][j])
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];
-  }
-result= matrix[i][j];                    
-if (result > 0){
-return 1; 
-}
-else{
-return 0  ; 
-} 
-}
+int i,j;
+int matrix[10][10];
 void claculate_short_path(int matrix[10][10]){
-int k,j,i;
-int result=0;
-for(k=0; k<=10 ; k++)
- for(i=1;i<=10;i++)
-  for(j=1;j<=10;j++)
+int k,i,j;
+for(k=0; k<=10 ; k++){
+ for(i=0;i<=10;i++){
+  for(j=0;j<=10;j++){
    if (matrix[i][k] + matrix[k][j] < matrix[i][j])
                     matrix[i][j] = matrix[i][k] + matrix[k][j];
-result= matrix[i][j];
-printf("%d" , result);   
+}
+}
+}
+}
+void getting_matrix(int matrix[10][10]){
+for(i=0;i<10;i++){
+  for(j=0;j<10;j++){
+  scanf("%d",&matrix[i][j]);
+  }
+}
+claculate_short_path(matrix);
+}
+int check_path(int matrix[10][10],int i,int j){
+if (matrix[i][j] == 0){
+return 0; 
+}
+else{
+return 1  ; 
+} 
+}
+
+int short_path(int matrix[10][10],int i,int j){
+int checking_path= check_path(matrix,i,j);
+if(check_path == 0){
+    return 0;
+}
+else{
+    
+    return matrix[i][j];
+}
 }
