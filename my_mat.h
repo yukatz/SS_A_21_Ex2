3a80743d8439f2a8dadbd03b095c14f04f0c5334
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-25 16:43:55 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,9 +1,8 @@
-#ifndef EX2
-#define EX2
+#include <stdio.h>
 
 void getting_matrix(int matrix[10][10]);
 int check_path(int matrix[10][10],int i, int j);
 void calculate_short_path();
 int short_path(int matrix[10][10],int i, int j);
 
-#endif
+
