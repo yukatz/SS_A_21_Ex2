857b0af92f5b1e8df4b86a6d648aa6282bd78761
avidan.abitbol <avidan88@walla.com>
2021-11-25 16:28:06 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,5 +1,9 @@
+#ifndef EX1
+#define EX1
+
 void getting_matrix(int matrix[10][10]);
 int check_path(int matrix[10][10],int i, int j);
 void calculate_short_path();
 int short_path(int matrix[10][10],int i, int j);
 
+#endif
