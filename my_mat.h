0716b098546451d8631b790dc98192616bad3c55
avidan.abitbol <avidan88@walla.com>
2021-11-25 16:25:55 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -2,6 +2,6 @@
 
 void getting_matrix(int matrix[10][10]);
 int check_path(int matrix[10][10],int i, int j);
-void calculate_short_path(int matrix[10][10]);
-int short_path(int matrix[10][10]);
+void calculate_short_path();
+int short_path(int matrix[10][10],int i, int j);
 
