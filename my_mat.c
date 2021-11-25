86eac3f45f18cf136617663be018b75fd7509c6d
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-25 18:18:46 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -2,14 +2,23 @@
 
   int i,j;
   int matrix[10][10];
-  
+  int findMin(int a,int b)
+  {
+      if(a<b){
+          return a;
+          
+      }
+      else {
+          return b;
+      }
+  }
      void claculate_short_path(int matrix[10][10]){
        int k,i,j;
-       for(k=0; k<=10 ; k++){
-         for(i=0;i<=10;i++){
-           for(j=0;j<=10;j++){
-               if (matrix[i][j]!=0 && matrix[i][k] !=0 && matrix[k][j] !=0 && matrix[i][k] + matrix[k][j] < matrix[i][j]){
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];}
+       for(k=0; k<10 ; k++){
+         for(i=0;i<10;i++){
+           for(j=0;j<10;j++){
+               if (matrix[i][j]!=0 && matrix[i][k] !=0 && matrix[k][j] !=0){
+                    matrix[i][j] = findMin(matrix[i][k]+ matrix[k][j],matrix[i][j]);}
                 if(matrix[i][j]==0 && i!=j && matrix[k][j]!=0 && matrix[i][k] !=0){
                     matrix[i][j]=matrix[i][k]+matrix[k][j];
                 }
@@ -23,6 +32,10 @@
         for(i=0;i<10;i++){
           for(j=0;j<10;j++){
             scanf("%d",&matrix[i][j]);
+            if(i==j){
+                matrix[i][j]=0;
+                
+            }
           }
         }
         claculate_short_path(matrix);
